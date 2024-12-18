fc79fdb669f3c2c3a324e1ebb79309e93136103b
Ethan Vijayabaskaran <ethan.vijayabaskaran@chevron.net>
2024-12-17 19:31:49 -0600

    trying

---
+++ todo-cli/target/debug/deps/todo_cli-f5de7a63818b85cc.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/zque/rust/todo/todo-cli/target/debug/deps/todo_cli-f5de7a63818b85cc: src/main.rs
+
+/mnt/c/Users/zque/rust/todo/todo-cli/target/debug/deps/todo_cli-f5de7a63818b85cc.d: src/main.rs
+
+src/main.rs:
---
+++ todo-cli/target/debug/todo-cli.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/zque/rust/todo/todo-cli/target/debug/todo-cli: /mnt/c/Users/zque/rust/todo/todo-cli/src/main.rs
